87eb364986ccc86895472f68eeec755dabd0e602
hellovijay <77140435+hellovijaykr@users.noreply.github.com>
2024-08-02 15:14:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcome to Devops" );
+        System.out.println( "Hello Edureka Team , Working on Assignment-2 ." );
+        System.out.println("Hii Madan , How are You ? ");
     }
 }
